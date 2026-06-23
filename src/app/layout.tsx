d2cfe60bdd0a44cac324c3e5ec2160a3849807d2
alirezagh99 <agh102724@gmail.com>
2026-06-23 17:25:56 +0330

    home layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${dmSans} h-full antialiased`}>
+    <html lang="en" className={`${dmSans.className} h-full antialiased`}>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
