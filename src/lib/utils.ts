e228b22e6d5aeeb32c31d6f46bf6daa30660f4e1
alirezagh99 <agh102724@gmail.com>
2026-07-16 18:18:47 +0330

    subdomain rewrites

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,8 +5,19 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+// export const generateTenantURL = (tenantSlug: string) => {
+//   return `/tenants/${tenantSlug}`;
+// };
+
 export const generateTenantURL = (tenantSlug: string) => {
-  return `/tenants/${tenantSlug}`;
+  if (process.env.NODE_ENV === "development") {
+    return `${process.env.NEXT_PUBLIC_APP_URL}/tenants/${tenantSlug}`;
+  }
+
+  const protocol = "https";
+  const domain = process.env.NEXT_PUBLIC_ROOT_DOMAIN!;
+
+  return `${protocol}://${tenantSlug}.${domain}`;
 };
 
 export function formatCurrency(value: number | string) {
